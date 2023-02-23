f8c4d805a74f2d43333ea1419ba1521b18c17edc
DIANELLY VENTURA <DMVCode@outlook.com>
2023-02-22 22:22:59 -0500

    Get data from web requests and refactor functions into seperate files

---
+++ src/PostsIndex.jsx
@@ -0,0 +1,17 @@
+export function PostsIndex(props) {
+  console.groupCollapsed(props);
+  return (
+    <div id="posts-index">
+      <h1>All posts</h1>
+      {props.posts.map((post) => (
+        <div key={post.id} className="posts">
+          <h2>{post.title}</h2>
+          <h3>{post.body}</h3>
+          <img src={post.image} alt="" />
+          <p>{post.user}</p>
+          <button>More Info</button>
+        </div>
+      ))}
+    </div>
+  );
+}
