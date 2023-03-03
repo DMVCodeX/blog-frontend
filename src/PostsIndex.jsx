a9ca90e52f7dce8adcf6b9c83142d46574e5e8bf
DIANELLY VENTURA <DMVCode@outlook.com>
2023-03-02 21:00:18 -0500

    Add search bar in posts index

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
+
 export function PostsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   return (
     <div id="posts-index">
       <h1>All posts</h1>
-      {props.posts.map((post) => (
-        <div key={post.id} className="posts">
-          <h2>{post.title}</h2>
-          <h3>{post.body}</h3>
-          <img src={post.image} alt="" />
-          <p></p>
-          <button type="button" className="btn btn-dark" onClick={() => props.onSelectPost(post)}>
-            More Info
-          </button>
-          <p></p>
-        </div>
-      ))}
+      Search: <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
+      {props.posts
+        .filter((post) => post.title.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((post) => (
+          <div key={post.id} className="posts">
+            <h2>{post.title}</h2>
+            <h3>{post.body}</h3>
+            <img src={post.image} alt="" />
+            <p></p>
+            <button type="button" className="btn btn-dark" onClick={() => props.onSelectPost(post)}>
+              More Info
+            </button>
+            <p></p>
+          </div>
+        ))}
     </div>
   );
 }
